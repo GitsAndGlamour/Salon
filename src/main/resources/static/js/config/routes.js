ba6f224e07ff99148529d15062871abbc21a9fc5
Omie Walls <omierwalls@gmail.com>
2016-10-20 23:37:58 -0400

    Cleaned and modified routing

--- src/main/resources/static/js/config/routes.js
+++ src/main/resources/static/js/config/routes.js
@@ -14,62 +14,38 @@ app.config(function($stateProvider, $urlRouterProvider) {
             templateUrl: $path+'home.html'
         })
 
-        .state('home.promotions.new', {
-            url: '/promotions/new',
-            templateUrl: $path+'promotions.new.html'
-        })
-
-        .state('home.customers.new', {
-            url: '/customers/new',
-            templateUrl: $path+'customers.new.html'
-        })
-
-        .state('home.calendar.new', {
-            url: '/calendar/new',
-            templateUrl: $path+'calendar.new.html'
-        })
-
-        .state('home.account', {
-            url: '/account/{id}',
-            templateUrl: $path+'home.account.html'
-        })
-
-        .state('home.login', {
-            url: '/login',
-            templateUrl: $path+'home.login.html'
-        })
-
-        // CALENDAR PAGE AND MULTIPLE NAMED VIEWS =================================
+        // CALENDAR PAGE AND MULTIPLE NAMED VIEWS ==============================
         .state('calendar', {
             url: '/calendar',
+            controller: 'CalendarCtrl',
             templateUrl: $path+'calendar.html'
         })
 
-        .state('calendar.new', {
-              url: '/calendar',
-              templateUrl: $path+'calendar.new.html'
-                })
-
-        // CUSTOMERS PAGE AND MULTIPLE NAMED VIEWS =================================
+        // CUSTOMERS PAGE AND MULTIPLE NAMED VIEWS =============================
         .state('customers', {
             url: '/customers',
+            controller: 'CustomersCtrl',
             templateUrl: $path+'customers.html'
         })
 
-        .state('customers.new', {
-            url: '/customers',
-            templateUrl: $path+'customers.new.html'
-        })
-
-        // REPORTS PAGE AND MULTIPLE NAMED VIEWS =================================
+        // REPORTS PAGE AND MULTIPLE NAMED VIEWS ===============================
         .state('reports', {
             url: '/reports',
+            controller: 'ReportsCtrl',
             templateUrl: $path+'reports.html'
         })
 
-        // SETTINGS PAGE AND MULTIPLE NAMED VIEWS =================================
+        // INVENTORY PAGE AND MULTIPLE NAMED VIEWS =============================
+        .state('inventory', {
+            url: '/inventory',
+            controller: 'InventoryCtrl',
+            templateUrl: $path+'inventory.html'
+        })
+
+        // SETTINGS PAGE AND MULTIPLE NAMED VIEWS ==============================
         .state('settings', {
             url: '/settings',
+            controller: 'SettingsCtrl',
             templateUrl: $path+'settings.html'
         });
 
